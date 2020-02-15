d8dd2405d33f4ca21751f5f82b2c3fb34ff59527
Fuhua Zhang <zhangfuhua@hipagesgroup.com.au>
2020-02-15 18:30:13 +0800

    feat: calculate position 2 fib

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,9 +1,17 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import java.util.ArrayList;
+
 public class Fibonacci {
     public int calculate(int position) {
         if(position < 1)
             throw new IllegalArgumentException();
-        return 1;
+
+        ArrayList<Integer> fibs = new ArrayList<>();
+        fibs.add(0);
+        fibs.add(1);
+        fibs.add(fibs.get(1)+fibs.get(0));
+
+        return fibs.get(position);
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -6,16 +6,23 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FibonacciTest {
+    Fibonacci fibonacci = new Fibonacci();
+
     @Test
     void should_return_1_when_calculate_given_position_1() {
-        Fibonacci fibonacci = new Fibonacci();
         int result = fibonacci.calculate(1);
         assertEquals(1, result);
     }
 
     @Test
     void should_throw_when_given_invalid_position() {
-        Fibonacci fibonacci = new Fibonacci();
         assertThrows(IllegalArgumentException.class, ()->fibonacci.calculate(-1));
+    }
+
+    @Test
+    void should_return_1_when_calculate_given_position_2() {
+        int result = fibonacci.calculate(2);
+        assertEquals(1, result);
+
     }
 }
