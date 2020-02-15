e917c739c0a45ef9c68084a7de9f57aabd08bd7b
Fuhua Zhang <zhangfuhua@hipagesgroup.com.au>
2020-02-15 19:33:40 +0800

    feat: output 50 fibs

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,9 +1,10 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Fibonacci {
-    public int calculate(int position) {
+    public long calculate(int position) {
         if(position < 1)
             throw new IllegalArgumentException();
 
@@ -11,6 +12,12 @@ public class Fibonacci {
             return 1;
         else
             return calculate(position-1) + calculate(position-2);
+    }
 
+    public long[] output() {
+        long[] fibs = new long[50];
+        IntStream.rangeClosed(1, 50).forEach(i -> fibs[i - 1] = calculate(i));
+        System.out.println(Arrays.toString(fibs));
+        return fibs;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -2,6 +2,10 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -10,7 +14,7 @@ public class FibonacciTest {
 
     @Test
     void should_return_1_when_calculate_given_position_1() {
-        int result = fibonacci.calculate(1);
+        long result = fibonacci.calculate(1);
         assertEquals(1, result);
     }
 
@@ -21,13 +25,25 @@ public class FibonacciTest {
 
     @Test
     void should_return_1_when_calculate_given_position_2() {
-        int result = fibonacci.calculate(2);
+        long result = fibonacci.calculate(2);
         assertEquals(1, result);
     }
 
     @Test
     void should_return_2_when_calculate_given_position_3() {
-        int result = fibonacci.calculate(3);
+        long result = fibonacci.calculate(3);
         assertEquals(2, result);
+    }
+
+    @Test
+    void should_return_50_fibs_when_output_given_position_50() {
+        long[] expectFibs = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
+                987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393,
+                196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887,
+                9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141,
+                267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073L,
+                4807526976L, 7778742049L, 12586269025L};
+        long[] result = fibonacci.output();
+        assertArrayEquals(expectFibs, result);
     }
 }
